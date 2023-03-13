6aacf5997a42214261206f8a1f7bd4e2f64549a3
refractiOOn <etozhesotnikow@gmail.com>
2023-03-13 18:16:57 +0200

    initial commit

---
+++ CppServer/main.cpp
@@ -0,0 +1,5 @@
+#include "pch.hpp"
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
---
+++ CppServer/pch.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#include "WinSock2.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+#include <wil/resource.h>
